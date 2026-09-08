8d161ba33502c8f3f1d678ff02ac14cf25d92949
Kayleerich <93557771+Kayleerich@users.noreply.github.com>
2026-09-08 12:30:29 -0600

    Update mk_kmers.py
    update licensing info

--- scripts/mk_kmers.py
+++ scripts/mk_kmers.py
@@ -1,3 +1,9 @@
+# ===============================================================================
+# mk_kmers.py : call functions to create k-mers file for mimicDetector
+# Copyright (C) 2024 Kaylee D. Rich
+# Read the COPYING file for license information.
+# ================================================================================
+
 from psychoscope import Chipset
 
 config_dict = snakemake.config
